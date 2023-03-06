6046822cc2e1ac90bcd70121deb667a0955a7736
v7153623 <71223238+v7153623@users.noreply.github.com>
2023-03-06 12:53:58 +0900

    {🎉}[S08P22A208-43]feat: 프로젝트 생성
    프로젝트 생성

---
+++ backend/b612/src/main/java/com/god/b612/B612Application.java
@@ -0,0 +1,13 @@
+package com.god.b612;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B612Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B612Application.class, args);
+	}
+
+}
