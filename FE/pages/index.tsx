cb7a9623af9f044f163ee7f0724952fc8744cad7
하훈목 <hunmok1027@gmail.com>
2023-03-10 14:46:58 +0900

    ✨ [S08P22A208-18] feat: profile 링크 추가

--- FE/pages/index.tsx
+++ FE/pages/index.tsx
@@ -4,11 +4,13 @@ import styles from '../styles/Home.module.css';
 import { useRecoilValue } from 'recoil';
 import userAtom from 'store/userAtom';
 import Example from '@components/Example';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   const user = useRecoilValue(userAtom);
   return (
     <div className={styles.container}>
+      <Link href={`/profile`}>myProfile</Link>
       <Example />
       {user.length == 0 ? <MetaMaskLogin /> : <h1>{user}</h1>}
     </div>
