ed82e224abe170239e553a4678126cd38765f7ef
yujinseo <00yujin00@naver.com>
2023-03-15 17:58:23 +0900

    ✨  [S08P22A208-86] feat: 광장 페이지 요소 추가

--- FE/pages/square/index.tsx
+++ FE/pages/square/index.tsx
@@ -2,19 +2,27 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 // import { PerspectiveCamera } from '@react-three/drei';
 import { Stars } from '@react-three/drei';
+import AnimatedBox from '@components/square/AnimatedBox';
 
 function Square() {
+  const styles = {
+    container: {
+      width: '100%',
+      height: '100vh',
+    },
+  } as const;
+
   return (
-    <div>
+    <div style={styles.container}>
       <Canvas>
+        <Stars />
         <ambientLight intensity={0.1} />
         <directionalLight color="navy" position={[0, 0, 5]} />
-        <mesh>
+        {/* <mesh>
           <boxGeometry />
           <meshStandardMaterial />
-        </mesh>
-
-        <Stars />
+        </mesh> */}
+        <AnimatedBox />
       </Canvas>
     </div>
   );
