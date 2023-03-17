ebcb7f46f842eeb3fd3627d033658fa6494a5dec
조우진 <sky5962146@gmail.com>
2023-03-17 10:51:07 +0900

    ✨[S08P22A208-107] feat: 실시간 채팅 WebSocket 기본구조 추가

---
+++ backend/b612/src/main/java/com/god/b612/handler/WebSocketChatHandler.java
@@ -0,0 +1,41 @@
+package com.god.b612.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class WebSocketChatHandler extends TextWebSocketHandler {
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();  //List를 전달할때 원본이 아닌 복사본을 만들어서 전달, 순회할 때 락이 필요 없어서 속도면에서 매우 빠름
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+//        String payLoad = message.getPayload();
+//        TextMessage textMessage = new TextMessage("echo: " + payLoad);
+        System.out.println("새로운 메시지: " + message.getPayload() + "세션id" + session.getId());
+        for(WebSocketSession s : sessions) {
+            if(!s.equals(session)) {  //다른 사용자들에게 채팅 전송
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    //새로운 WebSocket 연결
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        System.out.println("새로운 WebSocket 연결: " + session.getId());
+    }
+
+    //WebSocket 연결 종료
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        sessions.remove(session);
+        System.out.println("WebSocket 연결 종료" + session.getId());
+    }
+
+}
