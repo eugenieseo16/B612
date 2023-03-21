21ecada8d8eb8bed1768efad18f6f1379c897cc5
OYJ <yeonjinnieoh@gmail.com>
2023-03-21 17:50:58 +0900

    ✨ [S08P22A208-177] feat: 행성 상세 정보 카드

--- FE/pages/planet/index.tsx
+++ FE/pages/planet/index.tsx
@@ -1,39 +1,8 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import BlueGlowingButton from '@components/common/BlueGlowingButton';
-import PinkGlowingButton from '@components/common/PinkGlowingButton';
-import PurpleGlowingButton from '@components/common/PurpleGlowingButton';
-import YellowGlowingButton from '@components/common/YellowGlowingButton';
-
 function Planet() {
-  const FloatingButtons = styled.div`
-    position: fixed;
-    bottom: 50px;
-    right: 50px;
-
-    .floating-button-items {
-      padding: 1rem 0rem;
-    }
-  `;
-  return (
-    <FloatingButtons>
-      <div className="floating-button-items">
-        <BlueGlowingButton icon={'friend'} />
-      </div>
-
-      <div className="floating-button-items">
-        <PurpleGlowingButton icon={'plant'} />
-      </div>
-
-      <div className="floating-button-items">
-        <YellowGlowingButton icon={'certificate'} />
-      </div>
-      <div className="floating-button-items">
-        <PinkGlowingButton icon={'planet'} />
-      </div>
-    </FloatingButtons>
-  );
+  return <div></div>;
 }
 
 export default Planet;
