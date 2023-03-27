496f40765964af7807e44e62c54760d45678ea05
하훈목 <hunmok1027@gmail.com>
2023-03-27 15:34:32 +0900

    🔧 [S08P22A208-247] chore: react-query 세팅

--- FE/pages/_app.tsx
+++ FE/pages/_app.tsx
@@ -7,6 +7,8 @@ import Layout from '../components/Layout';
 import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PlanetController from '@components/PlanetController';
 React.useLayoutEffect = React.useEffect;
 
 declare global {
@@ -15,7 +17,7 @@ declare global {
     ethereum: any;
   }
 }
-
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const Background = styled.div`
@@ -29,36 +31,39 @@ function MyApp({ Component, pageProps }: AppProps) {
   `;
 
   return (
-    <RecoilRoot>
-      <UserController />
-      <Layout />
-      <AnimatePresence mode="wait">
-        <motion.div
-          style={{ minHeight: 'calc(100vh - 5rem)' }}
-          key={router.route}
-          initial="initialState"
-          animate="animateState"
-          exit="exitState"
-          transition={{ duration: 0.75, ease: 'easeInOut' }}
-          variants={{
-            initialState: {
-              opacity: 0,
-              clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
-            },
-            animateState: {
-              opacity: 1,
-              clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
-            },
-            exitState: {
-              clipPath: 'polygon(50% 0, 50% 0, 50% 100%, 50% 100%)',
-            },
-          }}
-        >
-          <Background />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <UserController />
+        <PlanetController />
+        <Layout />
+        <AnimatePresence mode="wait">
+          <motion.div
+            style={{ minHeight: 'calc(100vh - 5rem)' }}
+            key={router.route}
+            initial="initialState"
+            animate="animateState"
+            exit="exitState"
+            transition={{ duration: 0.75, ease: 'easeInOut' }}
+            variants={{
+              initialState: {
+                opacity: 0,
+                clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+              },
+              animateState: {
+                opacity: 1,
+                clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+              },
+              exitState: {
+                clipPath: 'polygon(50% 0, 50% 0, 50% 100%, 50% 100%)',
+              },
+            }}
+          >
+            <Background />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
