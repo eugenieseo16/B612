eebed488447c3a0e22aa346a1d67a6a04e9db702
yujinseo <00yujin00@naver.com>
2023-03-29 17:57:02 +0900

    ✨[S08P22A208-157] feat: Chatting 구현 중

--- FE/pages/chat/index.tsx
+++ FE/pages/chat/index.tsx
@@ -1,8 +1,32 @@
-// import Chatting from '@components/Chat/Chatting';
 import React from 'react';
 
-function Chat() {
-  return <div>{/* <Chatting /> */}</div>;
+class chat extends React.Component {
+  ws: any;
+
+  componentDidMount() {
+    this.ws = new WebSocket('wss://j8a208.p.ssafy.io/ws/chat');
+    this.ws.onopen = () => {
+      console.log('connected!!');
+    };
+    this.ws.onmessage = (evt: MessageEvent) => {
+      console.log(evt);
+      console.log(evt.data);
+    };
+  }
+
+  sendMessage = () => {
+    // 화살표함수로 만들것!!
+    this.ws.send('hello this is client Message'); // 서버로 메세지 보내는건 send
+  };
+
+  render() {
+    return (
+      <>
+        <div>Learn React</div>
+        <button onClick={this.sendMessage}>메세지 보내기</button>
+      </>
+    );
+  }
 }
 
-export default Chat;
+export default chat;
