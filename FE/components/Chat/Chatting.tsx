e4c24f81f7c7902a7ac9b9910a146ca018f106fb
yujinseo <00yujin00@naver.com>
2023-03-29 12:44:07 +0900

    ✨[S08P22A208-157] feat: Chatting 생성중

--- FE/components/Chat/Chatting.tsx
+++ FE/components/Chat/Chatting.tsx
@@ -1,41 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import * as io from 'socket.io-client';
+// import React, { useEffect, useState } from 'react';
+// import * as io from 'socket.io-client';
 
-const socket = io.connect('http://127.0.0.1:8080/api/chat');
+// const socket = io.connect('http://127.0.0.1:8080/chat');
 
-function Chatting() {
-  const [chat, setChat] = useState<string[]>([]);
-  const [message, setMessage] = useState('');
+// function Chatting() {
+//   const [chat, setChat] = useState<string[]>([]);
+//   const [message, setMessage] = useState('');
 
-  const sendMessageHandler = () => {
-    socket.emit('message', message);
-    setMessage('');
-  };
+//   const sendMessageHandler = () => {
+//     socket.emit('message', message);
+//     setMessage('');
+//   };
 
-  useEffect(() => {
-    socket.on('message', message => {
-      setChat([...chat, message]);
-    });
-  }, [chat]);
+//   useEffect(() => {
+//     socket.on('message', message => {
+//       setChat([...chat, message]);
+//     });
+//   }, [chat]);
 
-  return (
-    <div className="App">
-      <div>
-        <h1>Messages</h1>
-        <ul>
-          {chat.map((data, idx) => {
-            return <li key={idx}>{data}</li>;
-          })}
-        </ul>
-      </div>
+//   return (
+//     <div>
+//       <div>
+//         <h1>Messages</h1>
+//         <ul>
+//           {chat.map((data, idx) => {
+//             return <li key={idx}>{data}</li>;
+//           })}
+//         </ul>
+//       </div>
 
-      <div>
-        <h1>Chat Box</h1>
-        <input value={message} onChange={e => setMessage(e.target.value)} />
-        <button onClick={sendMessageHandler}>Send Message</button>
-      </div>
-    </div>
-  );
-}
+//       <div>
+//         <h1>Chat Box</h1>
+//         <input value={message} onChange={e => setMessage(e.target.value)} />
+//         <button onClick={sendMessageHandler}>Send Message</button>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Chatting;
+// export default Chatting;
+import WebSocket from 'ws';
