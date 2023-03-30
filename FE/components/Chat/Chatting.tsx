d60d2ebc77bb21d53a996c0a55964ca0008d810b
yujinseo <00yujin00@naver.com>
2023-03-30 17:54:02 +0900

    ✨[S08P22A208-157] feat: Chatting 구현 중

--- FE/components/Chat/Chatting.tsx
+++ FE/components/Chat/Chatting.tsx
@@ -1,16 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import styled from '@emotion/styled';
+
+interface Message {
+  content: string;
+  timestamp: number;
+  sentBy: 'user' | 'server';
+}
 
 const ChatContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: #000000;
+  opacity: 0.6;
 `;
 
 const ChatHeader = styled.header`
   padding: 20px;
-  background-color: #000000;
-  color: #fff;
+  color: #ffffff;
   width: 100%;
 `;
 
@@ -25,42 +32,63 @@ const ChatBody = styled.div`
   overflow-y: scroll;
 `;
 
-const MessageContainer = styled.div`
+interface MessageContainerProps {
+  sent: boolean;
+}
+
+const MessageContainer = styled.div<MessageContainerProps>`
   display: flex;
   justify-content: ${({ sent }) => (sent ? 'flex-end' : 'flex-start')};
   margin: 10px;
 `;
 
-const MessageBubble = styled.div`
-  padding: 10px;
+interface MessageBubbleProps {
+  sent: boolean;
+}
+
+const MessageBubble = styled.div<MessageBubbleProps>`
+  padding: 1rem;
   background-color: ${({ sent }) => (sent ? '#4CAF50' : '#ddd')};
   color: ${({ sent }) => (sent ? '#fff' : '#333')};
   border-radius: 10px;
   max-width: 70%;
 `;
 
+const ChatInputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1rem;
+  background-color: #000000;
+  opacity: 0.6;
+`;
+
 const ChatInput = styled.input`
-  margin-top: 20px;
-  padding: 10px;
+  background-color: #000000;
+  opacity: 0.6;
+  color: #ffffff;
+  margin-right: 0.5rem;
+  padding: 0.5rem;
   border: none;
   border-radius: 5px;
   width: 100%;
 `;
 
 const ChatButton = styled.button`
-  margin-top: 20px;
-  padding: 10px;
+  padding: 1rem;
   border: none;
   border-radius: 5px;
-  background-color: #4caf50;
-  color: #fff;
+  background-color: #ffffff;
+
+  color: #000;
   cursor: pointer;
 `;
 
-const Chat = () => {
-  const [messages, setMessages] = useState([]);
-  const [messageInput, setMessageInput] = useState('');
-  const [socket, setSocket] = useState(null);
+const Chat: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [messageInput, setMessageInput] = useState<string>('');
+  const [socket, setSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
     const ws = new WebSocket('wss://j8a208.p.ssafy.io/ws/chat');
@@ -70,7 +98,7 @@ const Chat = () => {
     };
 
     ws.onmessage = evt => {
-      const message = JSON.parse(evt.data);
+      const message: Message = JSON.parse(evt.data);
       setMessages(prevMessages => [...prevMessages, message]);
     };
 
@@ -84,44 +112,48 @@ const Chat = () => {
   const sendMessage = () => {
     if (messageInput.trim() === '') return;
 
-    const message = {
+    const message: Message = {
       content: messageInput,
       timestamp: Date.now(),
       sentBy: 'user',
     };
 
-    socket.send(JSON.stringify(message));
+    socket?.send(JSON.stringify(message));
     setMessages(prevMessages => [...prevMessages, message]);
     setMessageInput('');
   };
 
-  const handleInput = evt => {
+  const handleInput = (evt: ChangeEvent<HTMLInputElement>) => {
     setMessageInput(evt.target.value);
   };
 
   return (
-    <ChatContainer>
-      <ChatHeader>
-        <h1>Chat Room</h1>
-      </ChatHeader>
-      <ChatBody>
-        {messages.map((message, index) => (
-          <MessageContainer key={index} sent={message.sentBy === 'user'}>
-            <MessageBubble sent={message.sentBy === 'user'}>
-              {message.content}
-            </MessageBubble>
-          </MessageContainer>
-        ))}
-      </ChatBody>
-      <ChatInput
-        type="text"
-        placeholder="Type your message here"
-        value={messageInput}
-        onChange={handleInput}
-      />
-      <ChatButton onClick={sendMessage}>Send</ChatButton>
-    </ChatContainer>
+    <>
+      <ChatContainer>
+        <ChatHeader>
+          <p>아이콘 넣을거임</p>
+        </ChatHeader>
+        <ChatBody>
+          <hr />
+          {messages.map((message, index) => (
+            <MessageContainer key={index} sent={message.sentBy === 'user'}>
+              <MessageBubble sent={message.sentBy === 'user'}>
+                {message.content}
+              </MessageBubble>
+            </MessageContainer>
+          ))}
+        </ChatBody>
+      </ChatContainer>
+      <ChatInputContainer>
+        <ChatInput
+          type="text"
+          placeholder="채팅을 입력해주세요"
+          value={messageInput}
+          onChange={handleInput}
+        />
+        <ChatButton onClick={sendMessage}>Send</ChatButton>
+      </ChatInputContainer>
+    </>
   );
 };
-
 export default Chat;
