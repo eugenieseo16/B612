86e35d3d45ec50ffac7acb4c8277c16fde5e3fcb
yujinseo <00yujin00@naver.com>
2023-03-31 17:55:53 +0900

    ✨[S08P22A208-234] feat: 광장 통신 구현 중

--- FE/pages/square/index.tsx
+++ FE/pages/square/index.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Stats } from '@react-three/drei';
+import Chatting from '@components/Chat/Chatting';
 
 import {
   Lights,
