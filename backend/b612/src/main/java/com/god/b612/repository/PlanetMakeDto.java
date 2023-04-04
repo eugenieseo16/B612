2b0c50ff729c33c316c409bce6d88da9895e8e66
v7153623 <71223238+v7153623@users.noreply.github.com>
2023-04-04 17:02:26 +0900

    🐛 [S08P22A208-305] fix: 꽃 뽑기 안되던 것 수정

---
+++ backend/b612/src/main/java/com/god/b612/repository/PlanetMakeDto.java
@@ -0,0 +1,31 @@
+package com.god.b612.repository;
+
+import lombok.Builder;
+
+public class PlanetMakeDto {
+
+    int planetNftId;
+
+    int planetLikesCount;
+
+    String planetName;
+
+    int planetType;
+
+    int ownerMemberId;
+
+    boolean onSale;
+
+    String createdAt;
+
+    @Builder
+    public PlanetMakeDto(int ownerMemberId, int planetNftId, boolean onSale, String createdAt, int planetLikesCount,String planetName,int planetType){
+        this.planetNftId=planetNftId;
+        this.ownerMemberId=ownerMemberId;
+        this.onSale=onSale;
+        this.createdAt=createdAt;
+        this.planetLikesCount=planetLikesCount;
+        this.planetName=planetName;
+        this.planetType=planetType;
+    }
+}
