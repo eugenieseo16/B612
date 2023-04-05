6e24b351c791b3bae8c95156eee7c2fcaa6f636b
하훈목 <hunmok1027@gmail.com>
2023-04-05 23:55:12 +0900

    ✨ feat: 반응형 제작중

--- FE/components/Planet/FriendsModal.tsx
+++ FE/components/Planet/FriendsModal.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useState } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import userAtom from 'store/userAtom';
 
 import styled from '@emotion/styled';
@@ -15,10 +15,151 @@ import { useRouter } from 'next/router';
 import { useSearchByNameAPI } from 'API/memberAPIs';
 import axios from 'axios';
 import { friendAPIUrls } from 'API/apiURLs';
-import { Button } from '@mui/material';
+import { Button, Modal } from '@mui/material';
 import { tierDataList } from 'utils/tierDataList';
+import mainModalAtom from 'store/main/mainModalAtom';
 
-const Modal = styled.div`
+const FriendsModal = memo(function SomComponent() {
+  const user = useRecoilValue(userAtom);
+  const router = useRouter();
+  const data = useFriendAPI(user?.memberId);
+  const unresponse = useUnresponseFriend(user?.memberId);
+  const [search, setSearch] = useState('');
+  const searchResults = useSearchByNameAPI(search);
+  const [modalOpen, setModalOpen] = useRecoilState(mainModalAtom);
+
+  const ff: any = {};
+  searchResults?.responseData?.forEach((friend: IUser) => {
+    ff[friend.memberId] = 'not';
+  });
+
+  data?.responseData?.forEach(
+    (friend: IUser) => (ff[friend.memberId] = 'friend')
+  );
+  unresponse?.forEach((friend: IUser) => (ff[friend.memberId] = 'requested'));
+
+  const addFriend = (friendResponseMemberId: number) => {
+    axios.post(friendAPIUrls.requestFriendAPIUrl, {
+      friendRequestMemberId: user?.memberId,
+      friendResponseMemberId,
+    });
+  };
+
+  return (
+    <Modal
+      open={modalOpen.friend}
+      onClose={() => setModalOpen({ certificate: false, friend: false })}
+    >
+      <StyledModal>
+        {/* 검색바 */}
+        <Paper
+          component="form"
+          sx={{
+            p: '2px 4px',
+            display: 'flex',
+            alignItems: 'center',
+            width: '90%',
+            maxWidth: '100%',
+          }}
+        >
+          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="닉네임으로 검색해주세요"
+            inputProps={{ 'aria-label': '닉네임으로 검색해주세요' }}
+            onChange={e => setSearch(e.target.value)}
+            value={search}
+          />
+        </Paper>
+        {/* 검색결과 이미 친구인 사람은 return null*/}
+        {searchResults?.responseData?.map((friend: IUser) => {
+          if (ff[friend.memberId] === 'friend' || !search) return;
+          return (
+            <StrangerItem
+              key={friend.memberId}
+              className="friend"
+              onClick={() => router.push(`/profile/${friend.memberId}`)}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <img src={friend.memberImage} alt="" />
+                <div>
+                  <h3>{friend.memberNickname}</h3>
+                  <MemberDetail>
+                    <h6>{friend.memberTierName}</h6>
+                    <img
+                      src={tierDataList.get(friend.memberTierName)}
+                      alt="member tier"
+                      id="member-tier-icon"
+                      style={{
+                        width: '25px',
+                        height: '25px',
+                        marginLeft: '10px',
+                      }}
+                    />
+                  </MemberDetail>
+                </div>
+              </div>
+              <div>
+                {ff[friend.memberId] === 'not' ? (
+                  <Button
+                    color="success"
+                    variant="contained"
+                    onClick={() => addFriend(friend.memberId)}
+                  >
+                    <span style={{ color: '#fff' }}>친구추가</span>
+                  </Button>
+                ) : (
+                  <Button
+                    disabled
+                    variant="contained"
+                    onClick={() => addFriend(friend.memberId)}
+                  >
+                    <span style={{ color: 'grey' }}>수락대기중</span>
+                  </Button>
+                )}
+              </div>
+              {/* <Divider /> */}
+            </StrangerItem>
+          );
+        })}
+        {/* 친구 목록 */}
+        <h2 style={{ marginTop: '2rem' }}>친구 목록</h2>
+        {data?.responseData?.map((friend: IUser) => (
+          <ProfileItem
+            key={friend.memberId}
+            className="friend"
+            onClick={() => router.push(`/profile/${friend.memberId}`)}
+          >
+            <img src={friend.memberImage} alt="" />
+            <div>
+              <h3>{friend.memberNickname}</h3>
+              <MemberDetail>
+                <h6>{friend.memberTierName}</h6>
+                <img
+                  src={tierDataList.get(friend.memberTierName)}
+                  alt="member tier"
+                  id="member-tier-icon"
+                  style={{ width: '25px', height: '25px', marginLeft: '10px' }}
+                />
+              </MemberDetail>
+            </div>
+          </ProfileItem>
+        ))}
+      </StyledModal>
+    </Modal>
+  );
+});
+
+export default FriendsModal;
+
+const StyledModal = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -65,136 +206,3 @@ const ProfileItem = styled.div`
 const MemberDetail = styled.div`
   display: flex;
 `;
-const FriendsModal = memo(function SomComponent() {
-  const user = useRecoilValue(userAtom);
-  const router = useRouter();
-  const data = useFriendAPI(user?.memberId);
-  const unresponse = useUnresponseFriend(user?.memberId);
-  const [search, setSearch] = useState('');
-  const searchResults = useSearchByNameAPI(search);
-
-  const ff: any = {};
-  searchResults?.responseData?.forEach((friend: IUser) => {
-    ff[friend.memberId] = 'not';
-  });
-
-  data?.responseData?.forEach(
-    (friend: IUser) => (ff[friend.memberId] = 'friend')
-  );
-  unresponse?.forEach((friend: IUser) => (ff[friend.memberId] = 'requested'));
-
-  const addFriend = async (friendResponseMemberId: number) => {
-    const { data } = await axios.post(friendAPIUrls.requestFriendAPIUrl, {
-      friendRequestMemberId: user?.memberId,
-      friendResponseMemberId,
-    });
-  };
-
-  return (
-    <Modal>
-      {/* 검색바 */}
-      <Paper
-        component="form"
-        sx={{
-          p: '2px 4px',
-          display: 'flex',
-          alignItems: 'center',
-          width: '90%',
-          maxWidth: '100%',
-        }}
-      >
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="닉네임으로 검색해주세요"
-          inputProps={{ 'aria-label': '닉네임으로 검색해주세요' }}
-          onChange={e => setSearch(e.target.value)}
-          value={search}
-        />
-      </Paper>
-      {/* 검색결과 이미 친구인 사람은 return null*/}
-      {searchResults?.responseData?.map((friend: IUser) => {
-        if (ff[friend.memberId] === 'friend' || !search) return;
-        return (
-          <StrangerItem
-            key={friend.memberId}
-            className="friend"
-            onClick={() => router.push(`/profile/${friend.memberId}`)}
-          >
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-              }}
-            >
-              <img src={friend.memberImage} alt="" />
-              <div>
-                <h3>{friend.memberNickname}</h3>
-                <MemberDetail>
-                  <h6>{friend.memberTierName}</h6>
-                  <img
-                    src={tierDataList.get(friend.memberTierName)}
-                    alt="member tier"
-                    id="member-tier-icon"
-                    style={{
-                      width: '25px',
-                      height: '25px',
-                      marginLeft: '10px',
-                    }}
-                  />
-                </MemberDetail>
-              </div>
-            </div>
-            <div>
-              {ff[friend.memberId] === 'not' ? (
-                <Button
-                  color="success"
-                  variant="contained"
-                  onClick={() => addFriend(friend.memberId)}
-                >
-                  <span style={{ color: '#fff' }}>친구추가</span>
-                </Button>
-              ) : (
-                <Button
-                  disabled
-                  variant="contained"
-                  onClick={() => addFriend(friend.memberId)}
-                >
-                  <span style={{ color: 'grey' }}>수락대기중</span>
-                </Button>
-              )}
-            </div>
-            {/* <Divider /> */}
-          </StrangerItem>
-        );
-      })}
-      {/* 친구 목록 */}
-      <h2 style={{ marginTop: '2rem' }}>친구 목록</h2>
-      {data?.responseData?.map((friend: IUser) => (
-        <ProfileItem
-          key={friend.memberId}
-          className="friend"
-          onClick={() => router.push(`/profile/${friend.memberId}`)}
-        >
-          <img src={friend.memberImage} alt="" />
-          <div>
-            <h3>{friend.memberNickname}</h3>
-            <MemberDetail>
-              <h6>{friend.memberTierName}</h6>
-              <img
-                src={tierDataList.get(friend.memberTierName)}
-                alt="member tier"
-                id="member-tier-icon"
-                style={{ width: '25px', height: '25px', marginLeft: '10px' }}
-              />
-            </MemberDetail>
-          </div>
-        </ProfileItem>
-      ))}
-    </Modal>
-  );
-});
-
-export default FriendsModal;
--- FE/pages/index.tsx
+++ FE/pages/index.tsx
@@ -1,70 +1,34 @@
-import React, { useState } from 'react';
-import styled from '@emotion/styled';
 import type { NextPage } from 'next';
-import { useRecoilValue } from 'recoil';
-import userAtom from 'store/userAtom';
 
 import TestCanvas from '@components/Main/TestCanvas';
 
-import Modal from '@mui/material/Modal';
-
-import BlueGlowingButton from '@components/common/BlueGlowingButton';
-import YellowGlowingButton from '@components/common/YellowGlowingButton';
-
 import { CertificateModal, FriendsModal } from '@components/Planet/index';
 import { Canvas } from '@react-three/fiber';
+import MainNav from '@components/Main/MainNav';
+import styled from '@emotion/styled';
 
 const Home: NextPage = () => {
-  const FloatingButtons = styled.div`
-    position: fixed;
-    bottom: 3rem;
-    right: 3rem;
-
-    .floating-button-items {
-      padding: 1rem 0rem;
-    }
-  `;
-
-  const [openFriends, setOpenFriends] = useState(false);
-  const handleOpenFriends = () => setOpenFriends(true);
-  const handleCloseFriends = () => setOpenFriends(false);
-
-  const [openCertificate, setOpenCertificate] = useState(false);
-  const handleOpenCertificate = () => setOpenCertificate(true);
-  const handleCloseCertificate = () => setOpenCertificate(false);
-
   return (
-    <div style={{ paddingTop: '4rem' }}>
-      {/* {!Boolean(user) ? (
-        <></>
-      ) : (
-        <h3>{user?.memberNickname + '님, 환영해요!'}</h3>
-      )} */}
+    <Container>
       <Canvas style={{ width: '100vw', height: 'calc(100vh - 4rem)' }}>
         <TestCanvas />
       </Canvas>
 
-      <FloatingButtons>
-        <div className="floating-button-items" onClick={handleOpenFriends}>
-          <BlueGlowingButton icon={'friend'} />
-        </div>
-
-        <div className="floating-button-items" onClick={handleOpenCertificate}>
-          <YellowGlowingButton icon={'certificate'} />
-        </div>
-      </FloatingButtons>
-
+      <MainNav />
       {/* 친구 목록 조회 */}
-      <Modal open={openFriends} onClose={handleCloseFriends}>
-        <FriendsModal />
-      </Modal>
+      <FriendsModal />
 
       {/* 인증서 발급 */}
-      <Modal open={openCertificate} onClose={handleCloseCertificate}>
-        <CertificateModal />
-      </Modal>
-    </div>
+      <CertificateModal />
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  padding-top: 4rem;
+  @media (max-width: 500px) {
+    padding-top: 0;
+  }
+`;
