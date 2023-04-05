60694691d336c0331d3b7acddf758927d8383b4f
하훈목 <hunmok1027@gmail.com>
2023-04-05 16:57:05 +0900

    ✨ feat: 행성 최신화

--- FE/components/Main/PlanetById.tsx
+++ FE/components/Main/PlanetById.tsx
@@ -1,12 +1,13 @@
 import { Center, Html, useGLTF } from '@react-three/drei';
+import { useRouter } from 'next/router';
 import { colors } from 'styles/colors';
 import { Box3, Vector3 } from 'three';
 import { planetNameParser } from 'utils/planetUtil';
 import { PLANETS_LIST } from 'utils/utils';
 
 const PlanetById = ({ planet }: { planet: IPlanet }) => {
+  const router = useRouter();
   const { scene } = useGLTF(PLANETS_LIST[+planet.planetType]);
-  console.log(planet);
 
   //3D 모델링 리사이즈
   const bbox = new Box3().setFromObject(scene);
@@ -21,28 +22,32 @@ const PlanetById = ({ planet }: { planet: IPlanet }) => {
   scene.position.copy(center).multiplyScalar(-1);
 
   return (
-    <Center position={[6, 4, 0]}>
-      <group>
-        <primitive object={scene} />
-        <Html position={[0, -1, 0]}>
-          <div
-            style={{
-              width: '10rem',
-              backgroundColor: colors.purple,
-              padding: '1rem',
-              borderRadius: '1rem',
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <p style={{ color: '#fff' }}>
-              {planetNameParser(planet.planetName)[1]}
-            </p>
-            <span></span>
-          </div>
-        </Html>
-      </group>
-    </Center>
+    <>
+      <Center position={[6, 4, 0]}>
+        <group>
+          <primitive object={scene} />
+        </group>
+      </Center>
+      <Html position={[6, 4, 0]}>
+        <div
+          onClick={() => router.push(`/planet/${planet.planetTokenId}`)}
+          style={{
+            cursor: 'pointer',
+            width: '13rem',
+            backgroundColor: colors.purple,
+            padding: '1rem',
+            borderRadius: '1rem',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <p style={{ color: '#fff' }}>
+            {planetNameParser(planet.planetName)[1]}에 머무르기🌏
+          </p>
+          <span></span>
+        </div>
+      </Html>
+    </>
   );
 };
 export default PlanetById;
--- FE/components/Main/Square.tsx
+++ FE/components/Main/Square.tsx
@@ -1,8 +1,11 @@
-import { Center, useGLTF } from '@react-three/drei';
+import { Center, Html, useGLTF } from '@react-three/drei';
+import { useRouter } from 'next/router';
 import React from 'react';
+import { colors } from 'styles/colors';
 import { Box3, Vector3 } from 'three';
 
 function Square() {
+  const router = useRouter();
   const { scene } = useGLTF(
     'https://res.cloudinary.com/dohkkln9r/image/upload/v1680596386/square.glb'
   );
@@ -11,18 +14,36 @@ function Square() {
   const size = bbox.getSize(new Vector3());
 
   const maxAxis = Math.max(size.x, size.y, size.z);
-  scene.scale.multiplyScalar(10 / maxAxis);
+  scene.scale.multiplyScalar(12 / maxAxis);
   bbox.setFromObject(scene);
   bbox.getCenter(center);
   bbox.getSize(size);
   scene.position.copy(center).multiplyScalar(-1);
 
   return (
-    <Center position={[0, -1, 0]}>
-      <group>
-        <primitive object={scene} />
-      </group>
-    </Center>
+    <>
+      <Center position={[0, -1, 0]}>
+        <group>
+          <primitive object={scene} />
+        </group>
+      </Center>
+      <Html position={[4, -1, 0]}>
+        <div
+          onClick={() => router.push('/square')}
+          style={{
+            cursor: 'pointer',
+            width: '12rem',
+            backgroundColor: colors.purple,
+            padding: '1rem',
+            borderRadius: '1rem',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <p style={{ color: '#fff' }}>광장에 놀러가기🍹</p>
+        </div>
+      </Html>
+    </>
   );
 }
 
