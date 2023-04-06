80bf81c07ac30502d4fc6888df1d37bd617156a4
오연진 <yeonjinnieoh@gmail.com>
2023-04-07 01:08:01 +0900

    ✨ feat: 정원 title tag 추가

--- FE/utils/titleDataList.ts
+++ FE/utils/titleDataList.ts
@@ -5,3 +5,5 @@ titleDataList.set('store', '상점 | 머무르다 B612');
 titleDataList.set('square', '광장 | 머무르다 B612');
 titleDataList.set('planet', '행성 | 머무르다 B612');
 titleDataList.set('profile', '프로필 | 머무르다 B612');
+titleDataList.set('garden', '정원 | 머무르다 B612');
+
