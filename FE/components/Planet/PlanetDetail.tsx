048835335d9ec23ada19936c106b4040ab0e23e6
하훈목 <hunmok1027@gmail.com>
2023-04-06 16:40:07 +0900

    🐛 fix: 꽃구매후 바로 조회

--- FE/components/Planet/PlanetDetail.tsx
+++ FE/components/Planet/PlanetDetail.tsx
@@ -11,8 +11,10 @@ import dayjs from 'dayjs';
 
 import { usePlanetContract } from '@components/contracts/planetToken';
 import { planetNameParser } from 'utils/planetUtil';
-import { usePlanetDetailAPI, usePlanetOwnerAPI } from 'API/planetAPIs';
+import { usePlanetOwnerAPI } from 'API/planetAPIs';
 import { tierDataList } from 'utils/tierDataList';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -54,7 +56,36 @@ function PlanetDetailCard() {
 
   const handleClickOpen = () => {
     if (!user?.isApproved) {
-      alert('프로필에서 판매권한을 수락해주세요');
+      toast.error(
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+          }}
+        >
+          <p>판매권한을 수락해주세요</p>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => router.push(`/profile/${user?.memberId}`)}
+          >
+            <span style={{ color: '#fff' }}>프로필로 이동</span>
+          </Button>
+        </div>,
+        {
+          icon: null,
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+        }
+      );
+      // alert('프로필에서 판매권한을 수락해주세요');
       return;
     }
     setOpen(true);
@@ -134,101 +165,105 @@ function PlanetDetailCard() {
   const handlePurchase = () => {};
 
   return (
-    <PlanetDetail>
-      <div className="detail-container">
-        <div className="planet-name">
-          <h3>{adj}</h3>
-          <h2>{title}</h2>
-        </div>
-
-        <div
-          className="planet-owner"
-          onClick={() => router.push(`/profile/${ownerData?.memberId}`)}
-          style={{ cursor: 'pointer' }}
-        >
-          <img src={ownerData?.memberImage} alt="" />
-          <h6>{ownerData?.memberNickname}</h6>
-          <img
-            src={tierDataList.get(ownerData?.memberTierName)}
-            alt="member tier"
-            id="member-tier-icon"
-          />
-        </div>
-
-        <div className="planet-detail">
-          <div className="planet-date">
-            <p>
-              등록 날짜:
-              {planetDetail?.createdAt
-                ? dayjs(+planetDetail?.createdAt * 1000).format('YYYY-MM-DD')
-                : '0000-00-00'}
-            </p>
+    <>
+      <ToastContainer />
+      <PlanetDetail>
+        {/* Same as */}
+        <div className="detail-container">
+          <div className="planet-name">
+            <h3>{adj}</h3>
+            <h2>{title}</h2>
           </div>
-          {/* <div className="planet-price">
+
+          <div
+            className="planet-owner"
+            onClick={() => router.push(`/profile/${ownerData?.memberId}`)}
+            style={{ cursor: 'pointer' }}
+          >
+            <img src={ownerData?.memberImage} alt="" />
+            <h6>{ownerData?.memberNickname}</h6>
+            <img
+              src={tierDataList.get(ownerData?.memberTierName)}
+              alt="member tier"
+              id="member-tier-icon"
+            />
+          </div>
+
+          <div className="planet-detail">
+            <div className="planet-date">
+              <p>
+                등록 날짜:
+                {planetDetail?.createdAt
+                  ? dayjs(+planetDetail?.createdAt * 1000).format('YYYY-MM-DD')
+                  : '0000-00-00'}
+              </p>
+            </div>
+            {/* <div className="planet-price">
             <img src={Goerli.src} alt="Goerli Ethereum" id="goerli-ethereum" />
             <span>{planetPrice ? planetPrice * 10 ** -18 : 0} GETH</span>
           </div> */}
-        </div>
+          </div>
 
-        {user?.memberId === ownerData?.memberId ? (
-          // 본인 소유 행성일 때
-          <div className="for-sale-button">
-            {planetDetail?.onSale === false ? (
-              <button onClick={handleClickOpen}>팔기</button>
-            ) : (
-              <button onClick={handleDiscardSale}>팔기 취소</button>
-            )}
+          {user?.memberId === ownerData?.memberId ? (
+            // 본인 소유 행성일 때
+            <div className="for-sale-button">
+              {planetDetail?.onSale === false ? (
+                <button onClick={handleClickOpen}>팔기</button>
+              ) : (
+                <button onClick={handleDiscardSale}>팔기 취소</button>
+              )}
 
-            <Dialog open={open} onClose={handleClose}>
-              <DialogTitle> 판매를 희망하는 금액을 입력해주세요.</DialogTitle>
-              <DialogContent>
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="desired-amount"
-                  label="판매 희망 금액 (GETH)"
-                  fullWidth
-                  variant="standard"
-                  type="number"
-                  onChange={e => {
-                    setDesiredAmount(e.target.value);
-                    // 여기
+              <Dialog open={open} onClose={handleClose}>
+                <DialogTitle> 판매를 희망하는 금액을 입력해주세요.</DialogTitle>
+                <DialogContent>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="desired-amount"
+                    label="판매 희망 금액 (GETH)"
+                    fullWidth
+                    variant="standard"
+                    type="number"
+                    onChange={e => {
+                      setDesiredAmount(e.target.value);
+                      // 여기
+                    }}
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose}>취소</Button>
+                  <Button onClick={handleSale}>확인</Button>
+                </DialogActions>
+              </Dialog>
+            </div>
+          ) : (
+            // 타인 소유 행성일 때
+            <div className="friend-request-button">
+              {isFriend === 'notRequest' && !submit ? (
+                <FriendButton onClick={requestFriend}>친구신청</FriendButton>
+              ) : isFriend === 'notAccepted' || submit ? (
+                <FriendButton disabled style={{ background: 'grey' }}>
+                  수락대기중
+                </FriendButton>
+              ) : isFriend === 'friend' ? (
+                <Button
+                  style={{
+                    width: '3rem',
+                    backgroundColor: colors.yellow,
+                    borderRadius: '8px',
                   }}
-                />
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose}>취소</Button>
-                <Button onClick={handleSale}>확인</Button>
-              </DialogActions>
-            </Dialog>
-          </div>
-        ) : (
-          // 타인 소유 행성일 때
-          <div className="friend-request-button">
-            {isFriend === 'notRequest' && !submit ? (
-              <FriendButton onClick={requestFriend}>친구신청</FriendButton>
-            ) : isFriend === 'notAccepted' || submit ? (
-              <FriendButton disabled style={{ background: 'grey' }}>
-                수락대기중
-              </FriendButton>
-            ) : isFriend === 'friend' ? (
-              <Button
-                style={{
-                  width: '3rem',
-                  backgroundColor: colors.yellow,
-                  borderRadius: '8px',
-                }}
-              >
-                친구
-              </Button>
-            ) : null}
-            {planetDetail?.onSale && (
-              <button onClick={handlePurchase}>구매</button>
-            )}
-          </div>
-        )}
-      </div>
-    </PlanetDetail>
+                >
+                  친구
+                </Button>
+              ) : null}
+              {planetDetail?.onSale && (
+                <button onClick={handlePurchase}>구매</button>
+              )}
+            </div>
+          )}
+        </div>
+      </PlanetDetail>
+    </>
   );
 }
 
